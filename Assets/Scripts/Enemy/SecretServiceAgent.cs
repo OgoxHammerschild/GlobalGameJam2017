cd28ebcddebe0e6bc08c6e1a4f9dd53886240a8c
Daniel B. (OnSolaire) <OgoxHammerschild@users.noreply.github.com>
2017-01-22 00:17:12 +0100

    UPDATE: the SecretServiceAgent with a field of view and the ability to patrol

--- Assets/Scripts/Enemy/SecretServiceAgent.cs
+++ Assets/Scripts/Enemy/SecretServiceAgent.cs
@@ -6,17 +6,23 @@ using UnityEngine.AI;
 public class SecretServiceAgent : MonoBehaviour
 {
     public List<Transform> PatrolPoints = new List<Transform>();
+    public bool IsSleeping = false;
 
     [SerializeField]
     private float _stoppingDistance = 0.3f;
 
     private NavMeshAgent _agent;
-    private float _maxDistance = 5;
+    private float _maxPathDistance = 5;
     private WaveExpander _examinedSound;
     private Transform _myTransform;
 
     private Vector3 _nextPatrolPoint;
     private Queue<Vector3> _patrolPoints = new Queue<Vector3>();
+
+    private Transform _player;
+
+    [SerializeField]
+    private float _fieldOfView = 120;
 
     // Use this for initialization
     void Start()
@@ -33,17 +39,42 @@ public class SecretServiceAgent : MonoBehaviour
                 _agent.SetDestination(_nextPatrolPoint);
             }
         }
+
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
+        if (IsSleeping)
+            return;
+
+        if (!_agent)
+        {
+            print(name + " needs a NavMeshAgent-Component");
+            return;
+        }
+
+        if (CanSeePlayer())
+        {
+            _agent.SetDestination(_player.position);
+            return;
+        }
+        else if (_agent.destination != _nextPatrolPoint)
+        {
+            _agent.SetDestination(_nextPatrolPoint);
+        }
+
         if (_examinedSound)
         {
             if (Vector3.Distance(_myTransform.position, _examinedSound.transform.position) <= _stoppingDistance)
             {
-                if (_agent)
+                _examinedSound = null;
+                if (_patrolPoints.Count > 0)
                 {
-                    _examinedSound = null;
+                    _agent.SetDestination(_nextPatrolPoint);
+                }
+                else
+                {
                     _agent.Stop();
                 }
             }
@@ -52,7 +83,8 @@ public class SecretServiceAgent : MonoBehaviour
         {
             if (Vector3.Distance(_myTransform.position, _nextPatrolPoint) <= _stoppingDistance)
             {
-
+                _patrolPoints.Enqueue(_nextPatrolPoint);
+                _nextPatrolPoint = _patrolPoints.Dequeue();
             }
         }
     }
@@ -67,7 +99,7 @@ public class SecretServiceAgent : MonoBehaviour
 
             if (_agent.path.status == NavMeshPathStatus.PathComplete)
             {
-                if (_agent.remainingDistance > _maxDistance)
+                if (_agent.remainingDistance > _maxPathDistance)
                 {
                     _agent.Stop();
                 }
@@ -77,5 +109,34 @@ public class SecretServiceAgent : MonoBehaviour
                 }
             }
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private bool CanSeePlayer()
+    {
+        if (!_player)
+            return false;
+
+        RaycastHit hit;
+        Vector3 rayDirection = _player.position - _myTransform.position;
+
+        if (Mathf.Abs(Vector3.Angle(rayDirection, transform.forward)) <= _fieldOfView / 2)
+        {
+            if (Physics.Raycast(_myTransform.position, rayDirection, out hit, 2))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
