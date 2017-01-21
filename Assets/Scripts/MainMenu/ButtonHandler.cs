a35839a0a0a59948e3dbc897835b0e5d4cf243fd
Michael <luke-20@live.de>
2017-01-21 11:52:26 +0100

    ADDED: the Main Menu Buttons and some click funtion

---
+++ Assets/Scripts/MainMenu/ButtonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void OnOptionsButtonClick()
+    {
+        MainMenuEventHandler.F_OnPanelClick("P_Options");
+    }
+
+    public void OnBackButtonClick()
+    {
+        MainMenuEventHandler.F_OnPanelClick("Return");
+    }
+}
---
+++ Assets/Scripts/MainMenu/PanelHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelHandler : MonoBehaviour
+{
+    public GameObject P_MainMenu;
+    public GameObject P_Options;
+    public GameObject P_Credits;
+    GameObject _currentPanel;
+    GameObject _newPanel;
+    GameObject _lastPanel;
+
+    void Start()
+    {
+        _currentPanel = P_MainMenu;
+        SetupEvents();
+    }
+
+    private void SetupEvents()
+    {
+        MainMenuEventHandler.OnPanelClick += MainMenuEventHandler_OnPanelClick;
+    }
+
+    private void MainMenuEventHandler_OnPanelClick(string _newPanel)
+    {
+        SetNewPanel(_newPanel);
+        ChangePanel();
+    }
+
+    void SetNewPanel(string nPanel)
+    {
+        switch (nPanel)
+        {
+            case "P_Options":
+                _newPanel = P_Options;
+                break;
+            case "P_Credits":
+                _newPanel = P_Credits;
+                break;
+            case "P_MainMenu":
+                _newPanel = P_MainMenu;
+                break;
+            case "Return":
+                _newPanel = _lastPanel;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ChangePanel()
+    {
+        _lastPanel = _currentPanel;
+        _currentPanel.gameObject.SetActive(false);
+        _currentPanel = _newPanel;
+        _currentPanel.gameObject.SetActive(true);
+    }
+}
