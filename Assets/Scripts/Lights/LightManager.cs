52eaab75382da1ff98128e4d803f4a7c0bef06c9
Scr4py <kevinholst@gmx.net>
2017-01-22 12:55:08 +0100

    ADD: Event For Lights, Lightmanager new Light tag

---
+++ Assets/Scripts/Lights/LightManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour {
+
+
+    public bool Test;
+    public GameObject[] LightObject;
+    public SpriteRenderer[] LightRenderer;
+	// Use this for initialization
+	void Start ()
+    {
+        EventManager.Lights += TurnLightsOffAndOnAgain;
+        LightObject = GameObject.FindGameObjectsWithTag("Light");
+        for (int i = 0; i < LightObject.Length; i++)
+        {
+            LightRenderer[i] = LightObject[i].GetComponent<SpriteRenderer>();
+        }
+	}
+
+    private void TurnLightsOffAndOnAgain()
+    {
+        StartCoroutine(TurnAllTheLightsOffAndOn());
+    }
+
+    IEnumerator TurnAllTheLightsOffAndOn()
+    {
+        for (int i = 0; i < LightRenderer.Length; i++)
+        {
+            LightRenderer[i].enabled = false;
+        }
+        yield return new WaitForSeconds(.7f);
+        for (int i = 0; i < LightRenderer.Length; i++)
+        {
+            LightRenderer[i].enabled = true;
+        }
+        yield return new WaitForSeconds(.7f);
+        StartCoroutine(TurnAllTheLightsOffAndOn());
+    }
+}
---
+++ Assets/Scripts/UI/EventManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventManager
+{
+    public delegate void LightManager();
+    public static event LightManager Lights;
+
+
+    public static void F_Lights()
+    {
+        Lights();
+    }
+
+}
