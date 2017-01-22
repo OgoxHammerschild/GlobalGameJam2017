cb3f4df9fe6b8eedfe229c9f0759c34d422df159
Scr4py <kevinholst@gmx.net>
2017-01-22 12:09:09 +0100

    ADD: Atomic Trigger

---
+++ Assets/Scripts/PlayerStats/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+
+    public int Nukular;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Nukular >= 100)
+        {
+            //PlayerDead;
+        }	
+	}
+}
