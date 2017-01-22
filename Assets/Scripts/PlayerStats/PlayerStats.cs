79b7f46df48d6a89f80e6b4f382ddf4f649d26fb
Scr4py <kevinholst@gmx.net>
2017-01-22 13:14:47 +0100

    ADD:Deathscreen

--- Assets/Scripts/PlayerStats/PlayerStats.cs
+++ Assets/Scripts/PlayerStats/PlayerStats.cs
@@ -6,13 +6,37 @@ public class PlayerStats : MonoBehaviour {
 
 
     public int Nukular;
-	
-	// Update is called once per frame
-	void Update ()
+    public GameObject GObjectToDeactivate;
+    public GameObject RadiatedObject;
+    public GameObject ChasedObject;
+
+    void Start()
+    {
+        RadiatedObject.SetActive(false);
+        ChasedObject.SetActive(false);
+
+        EventManager.Chase += Chased;
+        EventManager.Radiaton += Radiated;
+    }
+
+    private void Radiated()
+    {
+        GObjectToDeactivate.SetActive(false);
+        RadiatedObject.SetActive(true);
+    }
+
+    private void Chased()
+    {
+        GObjectToDeactivate.SetActive(false);
+        ChasedObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         if (Nukular >= 100)
         {
-            //PlayerDead;
+            EventManager.F_Radiation();
         }	
 	}
 }
--- Assets/Scripts/UI/EventManager.cs
+++ Assets/Scripts/UI/EventManager.cs
@@ -7,10 +7,23 @@ public static class EventManager
     public delegate void LightManager();
     public static event LightManager Lights;
 
+    public delegate void GameOver();
+    public static event GameOver Radiaton, Chase;
+
 
     public static void F_Lights()
     {
         Lights();
     }
 
+
+    public static void F_Radiation()
+    {
+        Radiaton();
+    }
+
+    public static void F_Chase()
+    {
+        Chase();
+    }
 }
